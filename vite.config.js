645b98c84765a9fd8eef81b9333a244aef1f602b
JoeDev37 <alonejoe367@gmail.com>
2026-03-15 14:10:11 +0300

    encryption-decryption

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/encryption-decryption/",
+  base: "/text-encryption-decryption/",
 })
